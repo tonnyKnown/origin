5e76892e947b2cf39a59fa4620df84d1b457ddaf
tonnyKnown <duan_shizi@163.com>
2026-07-04 00:54:50 +0800

    提交

--- src/main/java/com/example/aiinterview/AiInterviewApplication.java
+++ src/main/java/com/example/aiinterview/AiInterviewApplication.java
@@ -1,12 +1,17 @@
 package com.example.aiinterview;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AiInterviewApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(AiInterviewApplication.class);
+
     public static void main(String[] args) {
+        log.info("项目启动中0.0");
         SpringApplication.run(AiInterviewApplication.class, args);
     }
 }
