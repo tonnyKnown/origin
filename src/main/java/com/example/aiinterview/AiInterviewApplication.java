287a4a450ba7918dce16061d02f1d9a7ff6578ad
tonnyKnown <duan_shizi@163.com>
2026-07-06 05:11:08 +0800

    新增用户画像功能--补充文件

--- src/main/java/com/example/aiinterview/AiInterviewApplication.java
+++ src/main/java/com/example/aiinterview/AiInterviewApplication.java
@@ -11,8 +11,7 @@ public class AiInterviewApplication {
     private static final Logger log = LoggerFactory.getLogger(AiInterviewApplication.class);
 
     public static void main(String[] args) {
-        log.info("项目启动中0.0");
         SpringApplication.run(AiInterviewApplication.class, args);
-        log.info("项目启动完成0.0");
+        log.info("项目启动完成############################");
     }
 }
